bf1d145dec33e9975ab534c749e5bfc761ee37f4
Anthony Alicea <alicea.9a@gmail.com>
2026-04-13 19:25:25 -0600

    Setup: Create Basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,112 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="bg-stone-300 w-screen h-screen flex justify-center items-center">
+      <div className="bg-gray-500 w-4/5 h-4/5 flex p-4 gap-4">
+        {/* Canvas and Palette */}
+
+        <div className="flex-1 flex flex-col gap-4">
+          {/* Canvas / Image */}
+          <div className="flex-1 bg-black flex items-center justify-center">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              className="object-cover w-full h-full"
+              src="/cet-image.png"
+              width={500}
+              height={500}
+              alt="Canvas"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+
+          {/* Palette */}
+          <div className="bg-gray-700 p-4 rounded">
+            <h2 className="text-center text-xl mb-2">Palette</h2>
+            <div className="flex justify-evenly">
+              <div className="border bg-red-400 p-4 rounded">color</div>
+              <div className="border bg-red-400 p-4 rounded">color</div>
+              <div className="border bg-red-400 p-4 rounded">color</div>
+              <div className="border bg-red-400 p-4 rounded">color</div>
+            </div>
+          </div>
         </div>
-      </main>
+
+        {/* Hover and Selected */}
+
+        <div className="flex-1 flex flex-col gap-5">
+          {/* Hover Color */}
+          <div
+            className={`bg-orange-400 flex flex-col p-4 rounded items-center`}
+          >
+            <div className="bg-blue-700 p-4 rounded-full w-24 h-24 flex items-center justify-center text-white">
+              Hover
+            </div>
+
+            <div className="w-full">
+              <h2 className="text-center text-xl mb-2">Color Info</h2>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between bg-gray-600 p-2 rounded items-center">
+                  <span>RGB</span>
+                  <span>#Val</span>
+                  <button className="p-1 border rounded cursor-pointer">
+                    Copy
+                  </button>
+                </div>
+                <div className="flex justify-between bg-gray-600 p-2 rounded items-center">
+                  <span>Hex</span>
+                  <span>#Val</span>
+                  <button className="p-1 border rounded cursor-pointer">
+                    Copy
+                  </button>
+                </div>
+                <div className="flex justify-between bg-gray-600 p-2 rounded items-center">
+                  <span>HSL</span>
+                  <span>#Val</span>
+                  <button className="p-1 border rounded cursor-pointer">
+                    Copy
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Selected Color */}
+          <div
+            className={`bg-green-500 flex flex-col p-4 rounded items-center gap-4`}
+          >
+            <div className="bg-blue-700 p-4 rounded-full w-24 h-24 flex items-center justify-center text-white">
+              Selected
+            </div>
+
+            <div className="w-full">
+              <h2 className="text-center text-xl mb-2">Color Info</h2>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between bg-gray-600 p-2 rounded items-center">
+                  <span>RGB</span>
+                  <span>#Val</span>
+                  <button className="p-1 border rounded cursor-pointer">
+                    Copy
+                  </button>
+                </div>
+                <div className="flex justify-between bg-gray-600 p-2 rounded items-center">
+                  <span>Hex</span>
+                  <span>#Val</span>
+                  <button className="p-1 border rounded cursor-pointer">
+                    Copy
+                  </button>
+                </div>
+                <div className="flex justify-between bg-gray-600 p-2 rounded items-center">
+                  <span>HSL</span>
+                  <span>#Val</span>
+                  <button className="p-1 border rounded cursor-pointer">
+                    Copy
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
