f2bfc008bcea73872e67332e2b37f08b4782a288
Anthony Alicea <alicea.9a@gmail.com>
2026-04-27 13:49:40 -0600

    WIP: Create getLargestColorRange to get largest color channel for palette feature

--- app/page.tsx
+++ app/page.tsx
@@ -68,6 +68,7 @@ export default function Home() {
   };
 
   const handleImageUpload = (e: any) => {
+    // UPDATE TYPE
     const file = e.target.files[0];
     console.log(file);
     if (file) {
@@ -76,9 +77,10 @@ export default function Home() {
   };
 
   const formatRgb = (imageData: any) => {
+    // UPDATE TYPE
 
     const rgbValues = [];
-    
+
     // Condense pixel data into rgb values by creating an rgb object on every 4th/255 element
     for (let i = 0; i < imageData.length; i += 4) {
       const rgb = {
@@ -88,10 +90,51 @@ export default function Home() {
       };
       rgbValues.push(rgb);
     }
-    return rgbValues;
+    return getLargestColorRange(rgbValues);
   };
 
+  const getLargestColorRange = (rgbValues: any) => {
+    // UPDATE TYPE
 
+    // Find Color Channel with largest Range
+
+    // Mins values
+    let rMin = Number.MAX_VALUE;
+    let gMin = Number.MAX_VALUE;
+    let bMin = Number.MAX_VALUE;
+
+    // Max values
+    let rMax = Number.MIN_VALUE;
+    let gMax = Number.MIN_VALUE;
+    let bMax = Number.MIN_VALUE;
+
+    // Loop through each pixel to compare and get each colors Min and Max values
+
+    rgbValues.map((pixel: any) => {
+      // UPDATE TYPE
+      rMin = Math.min(rMin, pixel.r);
+      gMin = Math.min(gMin, pixel.g);
+      bMin = Math.min(bMin, pixel.b);
+
+      rMax = Math.max(rMax, pixel.r);
+      gMax = Math.max(gMax, pixel.g);
+      bMax = Math.max(bMax, pixel.b);
+    });
+
+    // Get color range from min/max differences
+
+    const rRange = rMax - rMin;
+    const gRange = gMax - gMin;
+    const bRange = bMax - bMin;
+    const largestColorRange = Math.max(rRange, gRange, bRange);
+
+    // Return letter based on the largest color channel range
+    return rRange === largestColorRange
+      ? "r"
+      : gRange === largestColorRange
+        ? "g"
+        : "b";
+  };
 
   return (
     <div className="bg-stone-300 w-screen h-screen flex justify-center items-center">
