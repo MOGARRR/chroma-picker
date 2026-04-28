36e001e3452042f5a2d60ba8006c0dd502c6ab0e
Anthony Alicea <alicea.9a@gmail.com>
2026-04-28 13:50:48 -0600

    WIP: Add quantization function for palette feature

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import React, { useRef, useEffect, useState } from "react";
 import rgbToHex from "@/src/helpers/rgbToHex";
 import rgbToHsl from "@/src/helpers/rgbToHsl";
 
+type RGB = { r: number; g: number; b: number };
+
 export default function Home() {
   const [rgbHover, setRgbHover] = useState("rgb(0,0,0)");
   const [hexHover, setHexHover] = useState("#000000");
@@ -33,7 +35,8 @@ export default function Home() {
       ctx.drawImage(img, 0, 0);
 
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      console.log(formatRgb(imageData.data));
+      const rgbValues = formatRgb(imageData.data);
+      console.log(quantization(rgbValues, 1));
     };
   }, [uploadImage]);
 
@@ -90,7 +93,7 @@ export default function Home() {
       };
       rgbValues.push(rgb);
     }
-    return getLargestColorRange(rgbValues);
+    return rgbValues;
   };
 
   const getLargestColorRange = (rgbValues: any) => {
@@ -134,6 +137,55 @@ export default function Home() {
       : gRange === largestColorRange
         ? "g"
         : "b";
+  };
+
+  const quantization = (rgbValues: RGB[], paletteAmount: number): RGB[] => {
+
+    // Base Case
+
+    // Depth is 2^4 for 16 colors
+    const MAX_DEPTH = 4;
+
+    // When max depth is hit, return a color that is the average of the group
+    if (paletteAmount === MAX_DEPTH || rgbValues.length === 0) {
+      const color = rgbValues.reduce(
+        (prev: any, curr: any) => {
+          prev.r += curr.r;
+          prev.g += curr.g;
+          prev.b += curr.b;
+
+          return prev;
+        },
+        {
+          r: 0,
+          g: 0,
+          b: 0,
+        },
+      );
+
+      color.r = Math.round(color.r / rgbValues.length);
+      color.g = Math.round(color.g / rgbValues.length);
+      color.b = Math.round(color.b / rgbValues.length);
+      return [color];
+    }
+
+    // Recursion
+
+    const componentToSortBy = getLargestColorRange(rgbValues);
+
+
+    // sort pixels by channel to create color groups
+  
+    rgbValues.sort((a: { [x: string]: number }, b: { [x: string]: number }) => {
+      return a[componentToSortBy] - b[componentToSortBy];
+    });
+
+    const mid = rgbValues.length / 2;
+
+    return [
+      ...quantization(rgbValues.slice(0, mid), paletteAmount + 1),
+      ...quantization(rgbValues.slice(mid + 1), paletteAmount + 1),
+    ];
   };
 
   return (
